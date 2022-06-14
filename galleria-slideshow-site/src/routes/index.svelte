876638bb8b615fd43fbc9c4cc8e13f80b22bdeeb
theschmocker <jacobcs1996@gmail.com>
2022-06-13 19:53:31 -0500

    style and transition tweaks on home page

--- galleria-slideshow-site/src/routes/index.svelte
+++ galleria-slideshow-site/src/routes/index.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import PaintingCard from '$lib/components/PaintingCard.svelte';
 	import { useGallery } from '$lib/stores/gallery';
-	import { fade, fly } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 
 	const { columns } = useGallery();
 </script>
 
 <div
-	class="grid gap-[23px] p-6 items-start max-w-[1360px] mx-auto"
+	class="grid gap-[23px] p-6 md:p-10 md:gap-10 items-start max-w-[1360px] mx-auto"
 	style="grid-template-columns: repeat({$columns.length}, 1fr)"
-	in:fade={{ delay: 400 }}
-	out:fly={{ y: 25 }}
+	in:fly={{ delay: 400, y: -10 }}
+	out:fly={{ y: -10 }}
 >
 	{#each $columns as column}
-		<div class="grid gap-[23px]">
+		<div class="grid gap-[23px] md:gap-10">
 			{#each column as painting (painting.name)}
 				<PaintingCard {painting} />
 			{/each}
